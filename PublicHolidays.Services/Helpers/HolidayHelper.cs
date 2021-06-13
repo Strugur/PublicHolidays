23c1161f0550df4ccaa4d1213f547a8a455d08d5
Strugur <dleaaltiusa@gmail.com>
2021-06-13 13:30:44 +0300

    3

--- PublicHolidays.Services/Helpers/HolidayHelper.cs
+++ PublicHolidays.Services/Helpers/HolidayHelper.cs
@@ -12,6 +12,7 @@ namespace PublicHolidays.Services.Helpers
             {"Thursday",4},
             {"Friday",5},
             {"Saturday",6},
+            {"Saturday2",6},
             {"Sunday",7}
         };
       
